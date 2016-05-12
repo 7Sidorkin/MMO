243226c82d4bdaaeba7cddd167c15fd8b2c1091d
Mattcunningham777 <mattcunningham777@gmail.com>
2016-05-12 08:11:41 -0400

    Update game.java

--- game.java
+++ game.java
@@ -9,6 +9,38 @@ public class game extends Canvas implements Runnable{
 	public static final int HEIGHT = 720;
 	final String TITLE = "MMO";
 	
+	public void run(){
+		init();
+		long lastTime = System.nanoTime();
+		final double numberOfTicks = 60.0;
+		double ns = 1000000000 / numberOfTicks;
+		double delta = 0;
+		int updates = 0;
+		int frames = 0;
+		long timer = System.currentTimeMillis();
+		
+		while(running){
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			if(delta >= 1){
+				tick();
+				delta--;
+				updates++;
+			}
+			render();
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000){
+				timer += 1000;
+				System.out.println("Ticks: " + updates + "      Frames Per Second(FPS): " + frames);
+				updates = 0;
+				frames = 0;
+			}
+		}
+		stop();
+	}
+	
 	public static void main(String[] args) {
 		//Creates new instance of Game
 				
