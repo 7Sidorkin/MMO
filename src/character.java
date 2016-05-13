2f3c8d0464ec4acea5c0d6f596912cba02b67a05
Michael Cera <sidorkindaniel@gmail.com>
2016-05-13 09:06:55 -0400

    Add files via upload

--- src/character.java
+++ src/character.java
@@ -1,5 +1,4 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import java.util.Scanner;
 
 //Character selection.
 public class character {
@@ -8,5 +7,30 @@ public class character {
 	String[] shirt = { "white", "red", "blue", "yellow", "pink", "purple", "grey", "green" };
 	String[] pants = { "lt_blue", "dk_blue", "black", "white", "brown" };
 	String[] gender = { "xx", "xy" };
+	
+	//Each character must have the following:
+	static String chosenSkin;
+	static String chosenHair;
+	static String chosenShirt;
+	static String chosenPants;
+	static String chosenGender;
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Skin: ");
+		chosenSkin = scanner.nextLine();
+		System.out.println("Hair: ");
+		chosenHair = scanner.nextLine();
+		System.out.println("Shirt: ");
+		chosenShirt = scanner.nextLine();
+		System.out.println("Pants:");
+		chosenPants = scanner.nextLine();
+		System.out.println("Male or Female?:");
+		chosenGender = scanner.nextLine();
+		
+		System.out.println(chosenSkin + "\n" + chosenHair + "\n" + chosenShirt + "\n" + chosenPants + "\n" + chosenGender);
+		scanner.close();
+	}
 	
 }
