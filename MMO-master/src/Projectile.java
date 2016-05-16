b27ffcc159cb124c0acccc64c2e96c84ca32ec61
owen7900 <owen7900@scdsb.on.ca>
2016-05-16 18:58:24 -0400

    2 player functionality

--- MMO-master/src/Projectile.java
+++ MMO-master/src/Projectile.java
@@ -1,13 +1,17 @@
 import java.awt.Graphics;
+import java.awt.Rectangle;
 
 public class Projectile extends GameObject {
 	int speed = 10;
 	int numTick = 0;
+	int damage = 10;
+	private Rectangle arrow;
 
-	public Projectile(int x, int y, ID id) {
+	public Projectile(int x, int y, ID id, Player player) {
 		super(x, y, id);
-		this.motionX = (int) (speed* Math.cos(Math.toRadians(Game.game.player.pointing - 90)));
-		this.motionY = (int) (speed* Math.sin(Math.toRadians(Game.game.player.pointing - 90)));
+		this.motionX = (int) (speed* Math.cos(Math.toRadians(player.pointing - 90)));
+		this.motionY = (int) (speed* Math.sin(Math.toRadians(player.pointing - 90)));
+		this.arrow = new Rectangle(x, y, 5,5);
 	}
 
 	@Override
@@ -18,6 +22,16 @@ public class Projectile extends GameObject {
 		if(this.numTick > 65){
 			Game.game.handler.removeObject(this);
 		}
+		this.arrow.setLocation(x, y);
+		if(this.arrow.intersects(Game.game.player1.playerB)){
+			Game.game.player1.health -= damage;
+			Game.game.handler.removeObject(this);
+		}
+		if(this.arrow.intersects(Game.game.player2.playerB)){
+			Game.game.player2.health -= damage;
+			Game.game.handler.removeObject(this);
+		}
+		
 	}
 
 	@Override
