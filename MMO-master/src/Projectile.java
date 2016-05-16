95de96b39b241fb0d843e10b24ae0cb7bd9aaa04
Mattcunningham777 <mattcunningham777@gmail.com>
2016-05-16 09:06:37 -0400

    Update Projectile.java

--- MMO-master/src/Projectile.java
+++ MMO-master/src/Projectile.java
@@ -2,21 +2,26 @@ import java.awt.Graphics;
 
 public class Projectile extends GameObject {
 	int speed = 10;
+	int numTick = 0;
 
 	public Projectile(int x, int y, ID id) {
 		super(x, y, id);
-		motionX = (int) (speed* Math.cos(Math.toRadians(Game.game.player.pointing - 90)));
-		motionY = (int) (speed* Math.sin(Math.toRadians(Game.game.player.pointing - 90)));
+		this.motionX = (int) (speed* Math.cos(Math.toRadians(Game.game.player.pointing - 90)));
+		this.motionY = (int) (speed* Math.sin(Math.toRadians(Game.game.player.pointing - 90)));
 	}
 
 	@Override
 	public void tick() {
-		x += motionX;
-		y += motionY;
+		this.x += this.motionX;
+		this.y += this.motionY;
+		this.numTick++;
+		if(this.numTick > 65){
+			Game.game.handler.removeObject(this);
+		}
 	}
 
 	@Override
 	public void render(Graphics g) {
-		g.drawRect(x, y, 5, 5);
+		g.drawRect(this.x, this.y, 5, 5);
 	}
 }
